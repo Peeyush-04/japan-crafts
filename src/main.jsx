4f645112b5134dbbb6eab815d701e92b16ce5700
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-08 13:07:47 +0530

    Commit updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ThemeProvider>
+        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 )
